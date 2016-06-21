67b8965a39fd14ad595f8436ac2ec6a1e52b57a4
Stanislav Seletskiy <s.seletskiy@gmail.com>
2016-06-21 10:33:54 +0600

    remove useless usage of cut, fix #1

--- plugin.zsh
+++ plugin.zsh
@@ -3,11 +3,13 @@ favorite-directories:cd() {
     IFS=$'\n' eval 'local sources=($(favorite-directories:get))'
     local on_cd="favorite-directories:on-cd"
 
+    local name
+    local depth
+    local dir
+
     local target_dir=$({
         for source in "${sources[@]}"; do
-            local name=$(cut -f1 -d" " <<< "$source")
-            local depth=$(cut -f2 -d" " <<< "$source")
-            local dir=$(cut -f3- -d" " <<< "$source")
+            read -r name depth dir <<< "$source"
 
             find "$dir" -maxdepth "${depth:-1}" -type d \
                 -printf "$name: %P\\n"
@@ -18,8 +20,8 @@ favorite-directories:cd() {
     local target_dir=${target_dir//*: /}
 
     for source in "${sources[@]}"; do
-        local name=$(cut -f1 -d" " <<< "$source")
-        local dir=$(cut -f3- -d" " <<< "$source")
+        read -r name depth dir <<< "$source"
+
         if [ "$name" = "$token" ]; then
             eval cd "$dir/$target_dir"
             break
